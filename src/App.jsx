c19268c9dff2236eb3146cc968292f0ea5da7cb7
nooklesbr-droid <nooklesbr@gmail.com>
2026-04-02 22:06:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,6 @@ const databaseSecreto = [
       { name: "PAULO FRETEIRO", phone: "+55 41 9930-1081", notes: "Novo Freteiro" },
       { name: "JOHNNY FERNANDO", phone: "+55 45 9904-7330", notes: "Indicação Fernando" },
       { name: "DENISE FRETEIRA", phone: "+55 11 95722-2547", notes: "Indicação Fernando" },
-      { name: "JU RIBEIRO", phone: "+595 993 045009", notes: "Indicação Fernando" },
       { name: "ANTÔNIO", phone: "+1 (407) 574-1627", notes: "Indicação Fernando" },
       { name: "TIRZEFARMA", phone: "+595 992 171856", notes: "Freteiro" },
     ],
